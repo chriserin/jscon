0f701a21e67e7d54ab926f101cf9c80978062302
Christopher Erin <chris.erin@gmail.com>
2013-12-16 08:28:04 -0600

    Environment file ends with .rb

--- lib/jscon/session.rb
+++ lib/jscon/session.rb
@@ -3,7 +3,7 @@ module Jscon
     extend Forwardable
 
     def self.rails_app_path
-      File.join(::Dir.pwd, '/config/environment')
+      File.join(::Dir.pwd, '/config/environment.rb')
     end
 
     def self.is_available?
@@ -21,7 +21,9 @@ module Jscon
     end
 
     def configure_integration
+      #deliver stack trace instead of html error message
       Rails.application.config.action_dispatch.show_exceptions = false
+      #allow posts without session authentication tokens
       ActionController::Base.instance_eval do
         define_method :protect_against_forgery? do
           false
