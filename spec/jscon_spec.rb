61d55a13fd2950f6e53668401a72687553b73c6c
Chris Erin <dev+@hashrocke.com>
2014-05-01 15:01:16 -0500

    add coffee test and refactor

--- spec/jscon_spec.rb
+++ spec/jscon_spec.rb
@@ -2,14 +2,35 @@ require 'jscon'
 require 'ostruct'
 
 describe Jscon do
-  it 'should process js' do
-    repl = Jscon::Repl.new(options = OpenStruct.new(coffee?: false))
+  before do
     def repl.repl_write(value); value; end;
     repl.setup
-    sleep 1;
-    output = repl.process_input "1 + 2"
-    repl.teardown
+    sleep 0.6
+  end
 
-    expect(output).to eq '3'
+  let(:repl) {Jscon::Repl.new(options = OpenStruct.new(coffee?: coffee))}
+
+  context 'without coffee' do
+    let(:coffee) { false }
+
+    it 'should process js' do
+      output = repl.process_input "1 + 2"
+      expect(output).to eq '3'
+    end
+  end
+
+  context 'with coffee' do
+    let(:coffee) { true }
+
+    it 'should process coffee' do
+      repl.process_input "@a = 1 + 2"
+      repl.process_input "@b= -> @a"
+      output = repl.process_input "@b()"
+      expect(output).to eq '3'
+    end
+  end
+
+  after do
+    repl.teardown
   end
 end
